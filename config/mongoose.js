e79d472789478fa9c7c861eceaec89b0ed10461d
Aman Madd <29maddhesiaaman@gmail.com>
2023-03-18 16:33:45 +0530

    second commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,10 +2,11 @@ const mongoose=require("mongoose");
 
 const connectDB = async (DATABASE_URL)=>{
     try {
-        const DB_OPTIONS={
-            dbName:'Aman_Node_Auth_DB'
-        }
-        await mongoose.connect(DATABASE_URL,DB_OPTIONS);
+        // const DB_OPTIONS={
+        //     dbName:'Aman_Node_Auth_DB'
+        // }
+        // await mongoose.connect(DATABASE_URL,DB_OPTIONS);
+        await mongoose.connect(DATABASE_URL);
         console.log("Connected successfully to the database");
         
     } catch (error) {
